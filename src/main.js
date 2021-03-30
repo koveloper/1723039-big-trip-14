7e6038059167b287b4a14f9d238726b769a008b1
koveloper <koveloper@gmail.com>
2021-03-30 21:33:56 +0300

    add usage of all elements in main script

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+import {ViewElement} from './view/ViewElement.js';
 import {Menu} from './view/Menu.js';
 import {TripInfo} from './view/TripInfo.js';
+import {Filters} from './view/Filters.js';
+import {Sort} from './view/Sort.js';
+import {TripPointCreator} from './view/TripPointCreator.js';
+import {TripPointEditor} from './view/TripPointEditor.js';
+import {TripPoint} from './view/TripPoint.js';
 
 function renderElement(container, markupCode = '', placeToInsert = 'beforeEnd') {
   if (container) {
@@ -9,8 +15,21 @@ function renderElement(container, markupCode = '', placeToInsert = 'beforeEnd') 
 
 const menu = new Menu();
 const tripInfo = new TripInfo();
-
+const filters = new Filters();
+const sort = new Sort();
+const tripEventsList = new ViewElement('<ul class="trip-events__list"></ul>');
+const tripPointCreator = new TripPointCreator();
+const tripPointEditor = new TripPointEditor();
+const tripPoints = [new TripPoint('Saint-Petersburg'), new TripPoint('Arkhangelsk'), new TripPoint('Murmansk')];
 renderElement(document.querySelector('.trip-controls__navigation'), menu.markup);
 renderElement(document.querySelector('.trip-main'), tripInfo.markup, 'afterBegin');
+renderElement(document.querySelector('.trip-controls__filters'), filters.markup);
+renderElement(document.querySelector('.trip-events'), sort.markup);
+renderElement(document.querySelector('.trip-events'), tripEventsList.markup);
+renderElement(document.querySelector('.trip-events__list'), tripPointCreator.markup);
+renderElement(document.querySelector('.trip-events__list'), tripPointEditor.markup);
+for(const p of tripPoints) {
+  renderElement(document.querySelector('.trip-events__list'), p.markup);
+}
 
 
