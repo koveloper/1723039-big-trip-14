326ca77ec99142d19828c493e700529808dc5ff2
koveloper <koveloper@gmail.com>
2021-03-31 23:21:37 +0300

    update main script due to changes in modules

--- src/main.js
+++ src/main.js
@@ -1,35 +1,23 @@
-import {ViewElement} from './view/ViewElement.js';
-import {Menu} from './view/Menu.js';
-import {TripInfo} from './view/TripInfo.js';
-import {Filters} from './view/Filters.js';
-import {Sort} from './view/Sort.js';
-import {TripPointCreator} from './view/TripPointCreator.js';
-import {TripPointEditor} from './view/TripPointEditor.js';
-import {TripPoint} from './view/TripPoint.js';
-
-function renderElement(container, markupCode = '', placeToInsert = 'beforeEnd') {
-  if (container) {
-    container.insertAdjacentHTML(placeToInsert, markupCode);
-  }
-}
-
-const menu = new Menu();
-const tripInfo = new TripInfo();
-const filters = new Filters();
-const sort = new Sort();
-const tripEventsList = new ViewElement('<ul class="trip-events__list"></ul>');
-const tripPointCreator = new TripPointCreator();
-const tripPointEditor = new TripPointEditor();
-const tripPoints = [new TripPoint('Saint-Petersburg'), new TripPoint('Arkhangelsk'), new TripPoint('Murmansk')];
-renderElement(document.querySelector('.trip-controls__navigation'), menu.markup);
-renderElement(document.querySelector('.trip-main'), tripInfo.markup, 'afterBegin');
-renderElement(document.querySelector('.trip-controls__filters'), filters.markup);
-renderElement(document.querySelector('.trip-events'), sort.markup);
-renderElement(document.querySelector('.trip-events'), tripEventsList.markup);
-renderElement(document.querySelector('.trip-events__list'), tripPointCreator.markup);
-renderElement(document.querySelector('.trip-events__list'), tripPointEditor.markup);
-for(const p of tripPoints) {
-  renderElement(document.querySelector('.trip-events__list'), p.markup);
-}
+import { ViewElement } from './view/ViewElement.js';
+import { Menu } from './view/Menu.js';
+import { TripInfo } from './view/TripInfo.js';
+import { Filters } from './view/Filters.js';
+import { Sort } from './view/Sort.js';
+import { TripPointCreator } from './view/TripPointCreator.js';
+import { TripPointEditor } from './view/TripPointEditor.js';
+import { TripPoint } from './view/TripPoint.js';
+import { ViewValues } from './view/ViewValues.js';
 
 
+const viewItems = {
+  menu: new Menu(),
+  tripInfo: new TripInfo(),
+  filters: new Filters(),
+  sort: new Sort(),
+  tripEventsList: new ViewElement(ViewValues.selectors.EVENTS, '<ul class="trip-events__list"></ul>'),
+  tripPointCreator: new TripPointCreator(),
+  tripPointEditor: new TripPointEditor(),
+  tripPoints: [new TripPoint('Saint-Petersburg'), new TripPoint('Arkhangelsk'), new TripPoint('Murmansk')],
+};
+
+Object.values(viewItems).reduce((acc, el) => {return [...acc, ...(Array.isArray(el) ? el : [el])];}, []).forEach((el) => el.render());
