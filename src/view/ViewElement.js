2735a825d43a6925d02c90b4c8f168493bca86b7
koveloper <koveloper@gmail.com>
2021-03-31 23:20:23 +0300

    upgrade base ViewElement class with render method

--- src/view/ViewElement.js
+++ src/view/ViewElement.js
@@ -1,9 +1,21 @@
 export class ViewElement {
-  constructor(markup = '') {
+  constructor(containerSelector, markup = '', placeToInsert = 'beforeEnd') {
+    this.containerSelector_ = containerSelector;
     this.markup_ = markup;
+    this.placeToInsert_ = placeToInsert;
   }
 
   get markup() {
     return this.markup_;
+  }
+
+  get container() {
+    return this.containerSelector_ ? document.querySelector(this.containerSelector_) : null;
+  }
+
+  render() {
+    if (this.container) {
+      this.container.insertAdjacentHTML(this.placeToInsert_, this.markup);
+    }
   }
 }
