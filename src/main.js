21be99e9f3289016feb9db547a349e8d28eeec8c
koveloper <koveloper@gmail.com>
2021-04-11 18:56:50 +0300

    Refactor main.js to use ViewElements events

--- src/main.js
+++ src/main.js
@@ -13,12 +13,64 @@ import { ViewValues } from './constants.js';
 const testPoints = new Array(20).fill().map(() => generateTripPointData());
 
 const viewItemsWrappers = {
-  menu:  new ViewElementWrapper(ViewValues.selectors.MENU, new MenuView()),
+  menu: new ViewElementWrapper(ViewValues.selectors.MENU, new MenuView()),
   tripInfo: new ViewElementWrapper(ViewValues.selectors.INFO, new TripInfoView(testPoints), RenderUnit.getRenderPostions().AFTERBEGIN),
   filters: new ViewElementWrapper(ViewValues.selectors.FILTERS, new FiltersView()),
   sort: new ViewElementWrapper(ViewValues.selectors.SORT, new SortView()),
   tripEventsList: new ViewElementWrapper(ViewValues.selectors.EVENTS, new TripPointsContainerView()),
-  tripPoints: new ViewElementWrapper(ViewValues.selectors.POINT_CONTAINER, testPoints.map((e) => new TripPointView(e))),
+  tripPoints: testPoints.map((e) => new ViewElementWrapper(ViewValues.selectors.POINT_CONTAINER, new TripPointView(e))),
 };
 
-Object.values(viewItemsWrappers).forEach((we) => we.render());
+const tripPointsEditors = new Map();
+
+
+const switchToPointViewMode = (sourceWrapper) => {
+  for(const entry of tripPointsEditors) {
+    if(entry[1] === sourceWrapper) {
+      sourceWrapper.toggle(entry[0]);
+      return;
+    }
+  }
+};
+
+const switchToPointEditMode = (sourceWrapper) => {
+  if (!tripPointsEditors.has(sourceWrapper)) {
+    const editor = new ViewElementWrapper(ViewValues.selectors.POINT_CONTAINER, new TripPointEditorView(sourceWrapper.viewElement.tripPoint));
+    editor.viewElement.addEventListener(viewElementCallback);
+    tripPointsEditors.set(sourceWrapper, editor);
+  }
+  sourceWrapper.toggle(tripPointsEditors.get(sourceWrapper));
+};
+
+const getViewElementWrapper = (viewElement) => {
+  const finder = (wrapper) => {
+    return wrapper.viewElement === viewElement;
+  };
+  return Object.values(viewItemsWrappers).flatMap((viw) => viw).find(finder)
+    || [...tripPointsEditors.values()].find(finder);
+};
+
+const viewElementCallback = (e) => {
+  const sourceWrapper = getViewElementWrapper(e.source);
+  switch (e.type) {
+    case 'trip-point-edit':
+      switchToPointEditMode(sourceWrapper);
+      break;
+    case 'trip-point-edit-close':
+      switchToPointViewMode(sourceWrapper);
+      break;
+    case 'trip-point-save':
+      //TODO
+      break;
+    case 'trip-point-delete':
+      //TODO
+      break;
+    default:
+      break;
+  }
+};
+
+Object.values(viewItemsWrappers).flatMap((v) => v).forEach((el) => {
+  el.viewElement.addEventListener(viewElementCallback);
+  el.render();
+});
