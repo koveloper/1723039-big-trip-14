a44bf1269de9e04edaad99d8ce6a6627606d3fdc
koveloper <koveloper@gmail.com>
2021-04-17 22:19:02 +0300

    add favor click event type

--- src/view/handlers.js
+++ src/view/handlers.js
@@ -1,6 +1,7 @@
 export const handlerTypes = {
   OPEN_POINT_POPUP: 'open-point-popup',
   CLOSE_POINT_POPUP: 'close-point-popup',
+  FAVORITE_CLICK: 'favor-click',
   SAVE_POINT: 'save-point',
   DELETE_POINT: 'delete-point',
 };
