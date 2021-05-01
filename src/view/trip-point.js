35aae4aa7ece07f4937ee46fa8a6e15db3168d83
koveloper <koveloper@gmail.com>
2021-05-01 23:00:43 +0300

    bug fix in point view (event start date display)

--- src/view/trip-point.js
+++ src/view/trip-point.js
@@ -3,7 +3,7 @@ import { viewEvents } from './view-events.js';
 import { TimeUtils } from '../utils/time.js';
 
 const createDate = (from, to) => {
-  return `<time class="event__date" datetime="${TimeUtils.convertTo_YYYYMMDD(from)}">${TimeUtils.convertTo_MMMDD(to)}</time>`;
+  return `<time class="event__date" datetime="${TimeUtils.convertTo_YYYYMMDD(from)}">${TimeUtils.convertTo_MMMDD(from)}</time>`;
 };
 
 const createType = (type) => {
