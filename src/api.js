a6bd1aab91c33e8f519656e04c4bc880ceb32b06
koveloper <koveloper@gmail.com>
2021-05-03 23:50:14 +0300

    WIP. add adapter from/to backend data format

--- src/api.js
+++ src/api.js
@@ -10,7 +10,19 @@ const SuccessHTTPStatusRange = {
   MAX: 299,
 };
 
+const AdapterRools = {
+  'base_price': 'basePrice',
+  'date_from': 'dateFrom',
+  'date_to': 'dateTo',
+  'is_favorite': 'isFavorite',
+};
+
+const AdapterRoolsReversed = {};
+
+Object.keys(AdapterRools).forEach((backendKey) => AdapterRoolsReversed[AdapterRools[backendKey]] = backendKey);
+
 export default class Api {
+
   constructor(endPoint, authorization) {
     this._endPoint = endPoint;
     this._authorization = authorization;
@@ -18,7 +30,8 @@ export default class Api {
 
   getTripPoints() {
     return this._request({url: 'points'})
-      .then(Api.toJSON);
+      .then(Api.toJSON)
+      .then(Api.adaptToFront);
   }
 
   //   updateTask(task) {
@@ -54,8 +67,31 @@ export default class Api {
     ) {
       throw new Error(`${response.status}: ${response.statusText}`);
     }
-
     return response;
+  }
+
+  static _changeKeys(jsonObj, keysToChange) {
+    if(Array.isArray(jsonObj)) {
+      for(let i = 0; i < jsonObj.length; i++) {
+        jsonObj[i] = Api._changeKeys(jsonObj[i], keysToChange);
+      }
+      return jsonObj;
+    }
+    Object.keys(keysToChange).forEach((key) => {
+      if(key in jsonObj) {
+        jsonObj[keysToChange[key]] = jsonObj[key];
+        delete jsonObj[key];
+      }
+    });
+    return jsonObj;
+  }
+
+  static adaptToFront(jsonObj) {
+    return Api._changeKeys(jsonObj, AdapterRools);
+  }
+
+  static adaptToBack(jsonObj) {
+    return Api._changeKeys(jsonObj, AdapterRoolsReversed);
   }
 
   static toJSON(response) {
