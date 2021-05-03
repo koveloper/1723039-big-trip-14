7764886605b5a90ba53c24358e644742cdf98156
koveloper <koveloper@gmail.com>
2021-05-03 23:19:05 +0300

    WIP. create API class

---
+++ src/api.js
@@ -0,0 +1,68 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
+};
+
+const SuccessHTTPStatusRange = {
+  MIN: 200,
+  MAX: 299,
+};
+
+export default class Api {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getTripPoints() {
+    return this._request({url: 'points'})
+      .then(Api.toJSON);
+  }
+
+  //   updateTask(task) {
+  //     return this._load({
+  //       url: `tasks/${task.id}`,
+  //       method: Method.PUT,
+  //       body: JSON.stringify(task),
+  //       headers: new Headers({'Content-Type': 'application/json'}),
+  //     })
+  //       .then(Api.toJSON);
+  //   }
+
+  _request({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) {
+    headers.append('Authorization', this._authorization);
+
+    return fetch(
+      `${this._endPoint}/${url}`,
+      {method, body, headers},
+    )
+      .then(Api.checkStatus)
+      .catch(Api.catchError);
+  }
+
+  static checkStatus(response) {
+    if (
+      response.status < SuccessHTTPStatusRange.MIN ||
+      response.status > SuccessHTTPStatusRange.MAX
+    ) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    return response;
+  }
+
+  static toJSON(response) {
+    return response.json();
+  }
+
+  static catchError(err) {
+    throw err;
+  }
+}
