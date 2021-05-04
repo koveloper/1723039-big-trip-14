e3f5867789441b847723e390125a573901f908c3
koveloper <koveloper@gmail.com>
2021-05-05 00:05:08 +0300

    WIP. rebase no trip points component to use exiting DOM element and loading states

--- src/view/trip-points-container-empty.js
+++ src/view/trip-points-container-empty.js
@@ -1,11 +1,31 @@
 import AbstractViewElement from './abstract-view-element.js';
+import { ViewValues } from '../constants.js';
+
 
 export default class TripPointsContainerEmpty extends AbstractViewElement {
   constructor() {
     super();
+    this._isLoading = true;
+    this._isError = false;
+  }
+
+  setLoadingState(value) {
+    this._isLoading = value == ViewValues.loadStates.LOADING;
+    this._isError = value == ViewValues.loadStates.ERROR;
+  }
+
+  getElement() {
+    if(!this._element && document.querySelector('.trip-events__msg')) {
+      this._element = document.querySelector('.trip-events__msg');
+    }
+    return super.getElement();
   }
 
   getTemplate() {
-    return '<p class="trip-events__msg">Click New Event to create your first point</p>';
+    const sign =
+      this._isError ? 'Something goes wrong...<br>Try to connect later...' : (
+        this._isLoading ? 'Loading...' : 'Click New Event to create your first point'
+      );
+    return `<p class="trip-events__msg ${this._isError ? 'trip-events__msg__error' : ''}">${sign}</p>`;
   }
 }
